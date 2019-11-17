4ddf64ae3a24da1da6246d451b5a2a0dfcc76d0e
jowisaidmajn <joelsaidman1@gmail.com>
2019-11-16 21:56:31 -0300

    cambios menores en mensaje

--- src/Sem_Ascensor/Mensaje.h
+++ src/Sem_Ascensor/Mensaje.h
@@ -7,14 +7,30 @@ transmiten entre el ascensor y los pasajeros*/
 class Mensaje {
 private:
     int piso_actual=0;
-    bool esta_lleno=false; 
+    bool esta_lleno=false;
+    bool finalizo=false; 
 public:
     Mensaje()=default;
     ~Mensaje()=default;
-    int getPiso() { return this->piso_actual;};
-    void setPiso(int piso) {this->piso_actual = piso;};
-    bool getEstaLleno() { return this->esta_lleno;};
-    void setEstaLleno(bool esta_lleno) {this->esta_lleno=esta_lleno;};
+
+    int getPiso() { 
+        return this->piso_actual;
+    };
+
+    void setPiso(int piso) {
+        this->piso_actual = piso;
+    };
+
+    bool getEstaLleno() { 
+        return this->esta_lleno;
+    };
+
+    void setEstaLleno(bool esta_lleno) {
+        this->esta_lleno=esta_lleno;
+    };
+    void finalizar() {
+        this->finalizo=true;
+    }
 };
 
 #endif
